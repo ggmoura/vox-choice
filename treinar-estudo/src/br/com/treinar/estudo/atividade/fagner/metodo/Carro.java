d2d9ab518f93dc986a086edc71febead3e8afc79
Usuario <Usuario@TRE-BHZ-130>
2015-08-14 21:18:36 -0300

    Atividade de Herança

---
+++ treinar-estudo/src/br/com/treinar/estudo/atividade/fagner/metodo/Carro.java
@@ -0,0 +1,11 @@
+package br.com.treinar.estudo.atividade.fagner.metodo;
+
+public class Carro {
+	
+	public String modelo;
+	public String cor;
+	public Float motor;
+	
+	
+
+}
---
+++ treinar-estudo/src/br/com/treinar/estudo/atividade/fagner/metodo/CarroSport.java
@@ -0,0 +1,9 @@
+package br.com.treinar.estudo.atividade.fagner.metodo;
+
+public class CarroSport extends Carro {
+	
+	public String tipoDePneu;
+	public Boolean turbo;
+	
+
+}
