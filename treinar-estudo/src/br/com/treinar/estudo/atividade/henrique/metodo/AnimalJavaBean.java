be6843df1b20ec6cf82259215eba59403ccf2143
ADM <ADM@TRE-BHZ-136>
2015-08-17 20:42:41 -0300

    Atividade Encapsulamento

---
+++ treinar-estudo/src/br/com/treinar/estudo/atividade/henrique/metodo/AnimalJavaBean.java
@@ -0,0 +1,33 @@
+package br.com.treinar.estudo.atividade.henrique.metodo;
+
+public class AnimalJavaBean {
+	
+	private Integer tamanho;
+	private String habitat;
+	private String cor;
+	
+	public Integer getTamanho() {
+		return tamanho;
+	}
+
+	public void setTamanho(Integer tamanho) {
+		this.tamanho = tamanho;
+	}
+
+	public String getHabitat() {
+		return habitat;
+	}
+
+	public void setHabitat(String habitat) {
+		this.habitat = habitat;
+	}
+
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+}
