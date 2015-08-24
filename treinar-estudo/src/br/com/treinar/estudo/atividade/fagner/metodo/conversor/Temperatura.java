df8f55582c90b17ec706d6023d4a90dd31dfd88e
Cliente <Cliente@Cliente-PC>
2015-08-24 17:27:01 -0300

    Conversor

--- treinar-estudo/src/br/com/treinar/estudo/atividade/fagner/metodo/conversor/Temperatura.java
+++ treinar-estudo/src/br/com/treinar/estudo/atividade/fagner/metodo/conversor/Temperatura.java
@@ -2,78 +2,117 @@ package br.com.treinar.estudo.atividade.fagner.metodo.conversor;
 
 public class Temperatura {
 
-	private Double celsius;
-	private Double kelvin;
-	private Double fahrenheit;
+	private Float celsius;
+	private Float kelvin;
+	private Float fahrenheit;
 
-	public Double getCelsius() {
+	public Float getCelsius() {
 		return celsius;
 	}
 
-	public void setCelsius(Double celsius) {
+	public void setCelsius(Float celsius) {
 		this.celsius = celsius;
 	}
 
-	public Double getKelvin() {
+	public Float getKelvin() {
 		return kelvin;
 	}
 
-	public void setKelvin(Double kelvin) {
+	public void setKelvin(Float kelvin) {
 		this.kelvin = kelvin;
 	}
 
-	public Double getFahrenheit() {
+	public Float getFahrenheit() {
 		return fahrenheit;
 	}
 
-	public void setFahrenheit(Double fahrenheit) {
+	public void setFahrenheit(Float fahrenheit) {
 		this.fahrenheit = fahrenheit;
 	}
 
-	public Double converterCelsiusParaFahrenheit() {
-		Double resultado = null;
-			resultado = celsius * 1.8 + 32;
+	public Float converterCelsiusParaFahrenheit() {
+		Float resultado = null;
+			resultado = (float) (celsius * 1.8 + 32);
 	
 		return resultado;
 	}
+	public Float converterCelsiusParakelvin() {
+		Float resultado = null;
+		resultado = (float) (celsius + 273.15);
+		
+		return resultado;
 	
-	public Double converterTemperatura(Double temperatura, TipoTemperatura entrada, TipoTemperatura saida) {
+	}
+	
+	
+	
+	public Float converterKelvinParaFahrenheit() {
+		Float resultado = null;
+		resultado = (float) ((this.kelvin - 273.15) * 1.8 + 32);
 		
-		switch (entrada) {
-		case CELSIUS:
-			System.out.println("Escolha a temperatura de saida. ");
-			
-			switch (saida) {
-			case FAHRENHEIT:
-				Double resultado = null;
-				resultado = celsius * 1.8 + 32;
-				
-				return resultado;
-				
-			case KELVIN:
-				Double resultado2 = null;
-				resultado2 = celsius * 1.8 + 32;
-				
-				return resultado2;
-			default:
-				break;
-			
-			}
-			
-			break;
-		case KELVIN:
-			
-			break;
-		case FAHRENHEIT:
-			
-			break;
-
-		default:
-			break;
-		}
+		return resultado;
+	}
+	
+	public Float converterkelvinParaCelsius() {
+		Float resultado = null;
+		resultado = (float) (kelvin - 273.15);
 		
-		return celsius;
+		return resultado;
+		
+	}
+		
+	
+	
+	
+	public Float converterFahrenheitParaCelsius() {
+		Float resultado = null;
+		resultado = (float) ((fahrenheit - 32) / 1.8) ;
+		
+		return resultado;
+	}
+	public Float converterFahrenheitParakelvin() {
+		Float resultado = null;
+		resultado = (float) ((fahrenheit - 32) / 1.8 + 273.15) ;
+		
+		return resultado;
+	}
+	
+//	public Double converterTemperatura(Double temperatura, TipoTemperatura entrada, TipoTemperatura saida) {
+//		
+//		switch (entrada) {
+//		case CELSIUS:
+//			
+//			switch (saida) {
+//			case FAHRENHEIT:
+//				Double resultado = null;
+//				resultado = celsius * 1.8 + 32;
+//				
+//				return resultado;
+//				
+//			case KELVIN:
+//				Double resultado2 = null;
+//				resultado2 = celsius * 1.8 + 32;
+//				
+//				return resultado2;
+//			default:
+//				break;
+//			
+//			}
+//			
+//			break;
+//		case KELVIN:
+//			
+//			break;
+//		case FAHRENHEIT:
+//			
+//			break;
+//
+//		default:
+//			break;
+//		}
+//		
+//		return celsius;
 		
 	}
 	
-}
+
