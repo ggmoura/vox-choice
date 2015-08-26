003b6fcbbfce444905372597364f2cd518fc6d87
AS <AS@as2-PC>
2015-08-26 20:42:09 -0300

    Arquivos

---
+++ bb/src/br/com/treinar/estudo/atividade/bruno/metodo/DemoCalendar.java
@@ -0,0 +1,39 @@
+package br.com.treinar.estudo.atividade.bruno.metodo;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Scanner;
+
+public class DemoCalendar {
+	public static void main(String[] args) {
+		// ****Calendar****
+		Calendar nacimento = Calendar.getInstance();
+		nacimento.set(1996, 2, 14);
+		System.out.println(nacimento.getTime());
+		// ****Calendar****
+		Scanner leitor = new Scanner(System.in);
+		int dia,mes,ano;
+		System.out.println("Informe o dia de nacimento");
+		dia = leitor.nextInt();
+		System.out.println("Informe o mes de nacimento");
+		mes = leitor.nextInt();
+		System.out.println("Informe o ano de nacimento");
+		ano = leitor.nextInt();
+		data(dia, mes, ano);
+		leitor.close();
+	}
+
+	public static void data(int dia, int mes, int ano) {		
+		//**GregorianCalendar
+		String meses[] = {
+				"Janeiro", "Fevereiro", "Março", "April",
+				"Maio", "Junho", "Julho", "Agosto",
+				"Setembro", "Outubro", "Novenbro", "Dezembro"};
+		
+		Calendar nacimento2 = new GregorianCalendar(ano,mes-1,dia);
+		System.out.println(nacimento2.get(nacimento2.DAY_OF_MONTH)+" "+meses[nacimento2.MONTH]+" "+nacimento2.get(nacimento2.YEAR));
+		//**GregorianCalendar
+		
+	}
+
+}
---
+++ bb/src/br/com/treinar/estudo/atividade/bruno/metodo/HashMapDemo.java
@@ -0,0 +1,44 @@
+package br.com.treinar.estudo.atividade.bruno.metodo;
+
+import java.util.*;
+import java.util.Map.Entry;
+
+
+public class HashMapDemo {
+	public static void main(String[] args) {
+		Scanner leitor = new Scanner(System.in);
+		String nome = null;
+		Map<Character, List<String>> mapPessoa = new HashMap<>(); // HashMap<Chave,Oq vou  guarda>
+		List<String> nomes = null;  
+		// charAt()
+		do {
+			System.out.print("Informe o nome da pessoa: ");
+			nome = leitor.nextLine();
+			if (nome.equals("sair")) {
+				break;
+			}
+			// nome.charAt(0);
+			if (!mapPessoa.containsKey(nome.charAt(0))) { //.containskey (contem a key)
+				mapPessoa.put(nome.charAt(0), new ArrayList<String>());
+			}
+			nomes = mapPessoa.get(nome.charAt(0));
+			nomes.add(nome);
+		} while (!nome.equals("sair"));
+		leitor.close();
+		
+		Set<Entry<Character, List<String>>> listas = mapPessoa.entrySet();
+		for (Entry<Character, List<String>> chaves : listas) {
+			Collections.sort(chaves.getValue());
+		}
+		
+		int[] numeros = {1, 6, 8, 9};
+		
+		for (int i : numeros) {
+			System.out.println(i);
+		}
+		
+		
+		System.out.println(mapPessoa);
+	}
+
+}
