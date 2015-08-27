c26085838a9b3212df7321e6ed232d0e2c4616ff
AS <AS@as6>
2015-08-27 19:45:44 -0300

    Conversor

--- bb/src/br/com/treinar/estudo/atividade/fagner/metodo/conversor/Conversor.java
+++ bb/src/br/com/treinar/estudo/atividade/fagner/metodo/conversor/Conversor.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 
 public class Conversor {
 
-	public static Scanner Scanner;
+	public static Scanner leitor;
 
 	public Conversor() {
-		Scanner = new Scanner(System.in);
+		leitor = new Scanner(System.in);
 	}
 
 	public void iniciarSistema() {
@@ -15,7 +15,7 @@ public class Conversor {
 		System.out.println("Conversor de Unidades.\n\n");
 		System.out.println("Escolha a Unidade a ser convertida\n\n1-Temperatura \n2-Força \n3-Presão \n4-Energia");
 
-		Integer i = Scanner.nextInt();
+		Integer i = leitor.nextInt();
 
 		switch (i) {
 
@@ -43,16 +43,16 @@ public class Conversor {
 		System.out.println("1-Joules \n2-BTU \n3-Calorias ");
 		
 		Energia e = new Energia();
-		Integer i = Scanner.nextInt();
+		Integer i = leitor.nextInt();
 		
 		switch (i) {
 		case 1:
 			System.out.println("Informe a unidade de saida.");
 			System.out.println("1-BTU \n2-Calorias");
-			Integer i2 = Scanner.nextInt();
+			Integer i2 = leitor.nextInt();
 			
 			System.out.println("Informe o valor da energia de entrada.");
-			Float valor = Scanner.nextFloat();
+			Float valor = leitor.nextFloat();
 			
 			switch (i2) {
 			case 1:
@@ -73,10 +73,10 @@ public class Conversor {
 		case 2:
 			System.out.println("Informe a unidade de saida.");
 			System.out.println("1-Joules \n2-Calorias");
-			Integer i3 = Scanner.nextInt();
+			Integer i3 = leitor.nextInt();
 			
 			System.out.println("Informe o valor da energia de entrada.");
-			Float valor1 = Scanner.nextFloat();
+			Float valor1 = leitor.nextFloat();
 			
 			switch (i3) {
 			case 1:
@@ -97,10 +97,10 @@ public class Conversor {
 		case 3:
 			System.out.println("Informe a unidade de saida.");
 			System.out.println("1-Joules \n2-BTU");
-			Integer i4 = Scanner.nextInt();
+			Integer i4 = leitor.nextInt();
 			
 			System.out.println("Informe o valor da energia de entrada.");
-			Float valor2 = Scanner.nextFloat();
+			Float valor2 = leitor.nextFloat();
 			
 			switch (i4) {
 			case 1:
@@ -134,94 +134,30 @@ public class Conversor {
 	}
 
 	private static void temperatura() {
-		System.out.println("Escolha a temperatura de entrada.\n\n");
-		System.out.println("1-Celsius \n2-Kelvin \n3-Fahrenheit \n");
 
-		Scanner = new Scanner(System.in);
-		Temperatura t = new Temperatura();
-		Integer i = Scanner.nextInt();
-
-		System.out.println(recuperarMenu());
-
-		switch (i) {
-		case 1:
-			System.out.println("Escolha a temperatura de saida. ");
-			System.out.println("\n1-Kelvin \n2-Fahrenheit \n");
-
-			Integer i2 = Scanner.nextInt();
-			System.out.println("Digite o valor da temperatura de entrada.");
-			Float valor = Scanner.nextFloat();
-
-			switch (i2) {
-			case 1:
-				t.setCelsius(valor);
-				System.out.println("\nTemperatura Kelvin: " + t.converterCelsiusParakelvin() + "°K");
-				break;
-
-			case 2:
-				t.setCelsius(valor);
-				System.out.println("\nTemperatura Fahrenheit: " + t.converterCelsiusParaFahrenheit() + "°F");
-				break;
-
-			default:
-				break;
-
-			}
-			return;
-
-		case 2:
-			System.out.println("Escolha a temperatura de saida. ");
-			System.out.println("\n1-celsius \n2-Fahrenheit \n");
-
-			Integer i3 = Scanner.nextInt();
-			System.out.println("Digite o valor da temperatura de entrada.");
-			Float valor2 = Scanner.nextFloat();
-
-			switch (i3) {
-			case 1:
-				t.setKelvin(valor2);
-				System.out.println("\nTemperatura Fahrenheit: " + t.converterKelvinParaFahrenheit() + "°F");
-				break;
-
-			case 2:
-				t.setKelvin(valor2);
-				System.out.println("\nTemperatura Celsius: " + t.converterkelvinParaCelsius() + "°C");
-				break;
-
-			default:
-				break;
-			}
-
-			return;
-
-		case 3:
-			System.out.println("Escolha a temperatura de saida. ");
-			System.out.println("\n1-celsius \n2-kelvin \n");
-
-			Integer i4 = Scanner.nextInt();
-			System.out.println("Digite o valor da temperatura de entrada.");
-			Float valor3 = Scanner.nextFloat();
-
-			switch (i4) {
-			case 1:
-				t.setFahrenheit(valor3);
-				System.out.println("\nTemperatura Celsius: " + t.converterFahrenheitParaCelsius() + "°C");
-				break;
-
-			case 2:
-				t.setFahrenheit(valor3);
-				System.out.println("\nTemperatura Kelvin: " + t.converterFahrenheitParakelvin() + "°K");
-				break;
-
-			default:
-				break;
-
-			}
-
-			break;
-
+		Scanner leitor = new Scanner(System.in);
+		
+		TipoTemperatura[] tipos = TipoTemperatura.values();
+		
+		System.out.println("Temperaturas: ");
+		for (TipoTemperatura tipoTemperatura : tipos) {
+			System.out.println("\t" + (tipoTemperatura.ordinal() + 1) + " - " + tipoTemperatura);
 		}
+		
+		System.out.print("\n\nDigite a temperatura de entrada: ");
+		Integer entrada = leitor.nextInt();
+		
+		System.out.print("Digite o valor da temperatura de entrada: ");
+		Double temperatura = leitor.nextDouble();
 
+		System.out.print("Digite a temperatura de saida: ");
+		Integer saida = leitor.nextInt();
+
+		leitor.close();
+		
+		System.out.println(Temperatura.converterTemperatura(temperatura, tipos[entrada - 1], tipos[saida - 1]));
+
+		
 	}
 
 	public static String recuperarMenu() {
@@ -233,6 +169,10 @@ public class Conversor {
 					+ "4-Energia \n" 
 					+ "0-Sair";
 		return menu;
+	}
+	
+	public static void main(String[] args) {
+		temperatura();
 	}
 
 }
--- bb/src/br/com/treinar/estudo/atividade/fagner/metodo/conversor/Temperatura.java
+++ bb/src/br/com/treinar/estudo/atividade/fagner/metodo/conversor/Temperatura.java
@@ -2,117 +2,92 @@ package br.com.treinar.estudo.atividade.fagner.metodo.conversor;
 
 public class Temperatura {
 
-	private Float celsius;
-	private Float kelvin;
-	private Float fahrenheit;
-
-	public Float getCelsius() {
-		return celsius;
-	}
-
-	public void setCelsius(Float celsius) {
-		this.celsius = celsius;
-	}
-
-	public Float getKelvin() {
-		return kelvin;
-	}
-
-	public void setKelvin(Float kelvin) {
-		this.kelvin = kelvin;
-	}
-
-	public Float getFahrenheit() {
-		return fahrenheit;
-	}
-
-	public void setFahrenheit(Float fahrenheit) {
-		this.fahrenheit = fahrenheit;
-	}
-
-	public Float converterCelsiusParaFahrenheit() {
+	public static Float converterCelsiusParaFahrenheit(Double celsius) {
 		Float resultado = null;
-			resultado = (float) (celsius * 1.8 + 32);
-	
+		resultado = (float) (celsius * 1.8 + 32);
+
 		return resultado;
 	}
-	public Float converterCelsiusParakelvin() {
+
+	public static Float converterCelsiusParakelvin(Double celsius) {
 		Float resultado = null;
 		resultado = (float) (celsius + 273.15);
-		
+
 		return resultado;
-	
+
 	}
-	
-	
-	
-	public Float converterKelvinParaFahrenheit() {
+
+	public static Float converterKelvinParaFahrenheit(Double kelvin) {
 		Float resultado = null;
-		resultado = (float) ((this.kelvin - 273.15) * 1.8 + 32);
-		
+		resultado = (float) ((kelvin - 273.15) * 1.8 + 32);
+
 		return resultado;
 	}
-	
-	public Float converterkelvinParaCelsius() {
+
+	public static Float converterkelvinParaCelsius(Double kelvin) {
 		Float resultado = null;
 		resultado = (float) (kelvin - 273.15);
-		
-		return resultado;
-		
-	}
-		
-	
-	
-	
-	public Float converterFahrenheitParaCelsius() {
-		Float resultado = null;
-		resultado = (float) ((fahrenheit - 32) / 1.8) ;
-		
-		return resultado;
-	}
-	public Float converterFahrenheitParakelvin() {
-		Float resultado = null;
-		resultado = (float) ((fahrenheit - 32) / 1.8 + 273.15) ;
-		
-		return resultado;
-	}
-	
-//	public Double converterTemperatura(Double temperatura, TipoTemperatura entrada, TipoTemperatura saida) {
-//		
-//		switch (entrada) {
-//		case CELSIUS:
-//			
-//			switch (saida) {
-//			case FAHRENHEIT:
-//				Double resultado = null;
-//				resultado = celsius * 1.8 + 32;
-//				
-//				return resultado;
-//				
-//			case KELVIN:
-//				Double resultado2 = null;
-//				resultado2 = celsius * 1.8 + 32;
-//				
-//				return resultado2;
-//			default:
-//				break;
-//			
-//			}
-//			
-//			break;
-//		case KELVIN:
-//			
-//			break;
-//		case FAHRENHEIT:
-//			
-//			break;
-//
-//		default:
-//			break;
-//		}
-//		
-//		return celsius;
-		
-	}
-	
 
+		return resultado;
+
+	}
+
+	public static Float converterFahrenheitParaCelsius(Double fahrenheit) {
+		Float resultado = null;
+		resultado = (float) ((fahrenheit - 32) / 1.8);
+
+		return resultado;
+	}
+
+	public static Float converterFahrenheitParakelvin(Double fahrenheit) {
+		Float resultado = null;
+		resultado = (float) ((fahrenheit - 32) / 1.8 + 273.15);
+
+		return resultado;
+	}
+
+	public static Float converterTemperatura(Double temperatura,
+			TipoTemperatura entrada, TipoTemperatura saida) {
+		Float temperaturaSaida = null;
+
+		switch (entrada) {
+		case CELSIUS:
+			switch (saida) {
+			case FAHRENHEIT:
+				temperaturaSaida = converterCelsiusParaFahrenheit(temperatura);
+			case KELVIN:
+				temperaturaSaida = converterCelsiusParakelvin(temperatura);
+			default:
+				break;
+			}
+
+			break;
+		case KELVIN:
+			switch (saida) {
+			case FAHRENHEIT:
+				temperaturaSaida = converterKelvinParaFahrenheit(temperatura);
+			case CELSIUS:
+				temperaturaSaida = converterkelvinParaCelsius(temperatura);
+			default:
+				break;
+			}
+
+			break;
+		case FAHRENHEIT:
+			switch (saida) {
+			case CELSIUS:
+				temperaturaSaida = converterFahrenheitParaCelsius(temperatura);
+			case KELVIN:
+				temperaturaSaida = converterFahrenheitParakelvin(temperatura);
+			default:
+				break;
+			}
+			break;
+
+		default:
+			break;
+		}
+
+		return temperaturaSaida;
+	}
+}
