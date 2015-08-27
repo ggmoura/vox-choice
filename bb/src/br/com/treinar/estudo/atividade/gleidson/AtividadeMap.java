25c143701e98c5cf3ed2e405ffb6e000299329ac
ggmoura <gleidson.gmoura@gmail.com>
2015-08-26 21:10:20 -0300

    atividade arquivo

--- bb/src/br/com/treinar/estudo/atividade/gleidson/AtividadeMap.java
+++ bb/src/br/com/treinar/estudo/atividade/gleidson/AtividadeMap.java
@@ -1,16 +1,23 @@
 package br.com.treinar.estudo.atividade.gleidson;
 
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Scanner;
+import java.util.Set;
 
 public class AtividadeMap {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		Scanner sc = new Scanner(System.in);
-		Map<Character, List<String>> nomes = new HashMap<Character, List<String>>();
+		Map<Character, List<String>> nomesMap = new HashMap<Character, List<String>>();
 		String nome = null;
 		Character primeiraLetra = null;
 		List<String> listaNomesPorLetra = null;
@@ -19,15 +26,30 @@ public class AtividadeMap {
 			nome = sc.nextLine();
 			if (!nome.equals("sair")) {
 				primeiraLetra = nome.toUpperCase().charAt(0);
-				if (!nomes.containsKey(primeiraLetra)) {
-					nomes.put(primeiraLetra, new ArrayList<String>());
+				if (!nomesMap.containsKey(primeiraLetra)) {
+					nomesMap.put(primeiraLetra, new ArrayList<String>());
 				}
-				listaNomesPorLetra = nomes.get(primeiraLetra);
+				listaNomesPorLetra = nomesMap.get(primeiraLetra);
 				listaNomesPorLetra.add(nome);
 			}
-			System.out.println(nomes);
 			
 		} while (!nome.equals("sair"));
+		
+		OutputStream os = new FileOutputStream("saida-" + new Date().getTime() + ".txt");
+		OutputStreamWriter osw = new OutputStreamWriter(os);
+		BufferedWriter bw = new BufferedWriter(osw);
+		
+		Set<Entry<Character, List<String>>> entrySet = nomesMap.entrySet();
+		
+		for (Entry<Character, List<String>> entry : entrySet) {
+			for (String n : entry.getValue()) {
+				bw.write(n);
+				bw.newLine();
+			}
+			bw.newLine();
+		}
+		
+		bw.close();
 		sc.close();
 	}
 	
