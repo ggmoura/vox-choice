bb4877c1f543d33acfbab044d8a7655396eaf6cb
AS <AS@as6>
2015-08-31 19:42:49 -0300

    janela

---
+++ bb/src/br/com/treinar/estudo/atividade/fagner/metodo/Janela.java
@@ -0,0 +1,13 @@
+package br.com.treinar.estudo.atividade.fagner.metodo;
+
+import javax.swing.JOptionPane;
+
+public class Janela {
+	
+	public static void main(String[] args) {
+		
+		
+		JOptionPane.showMessageDialog(null, "Fagner");
+	}
+
+}
