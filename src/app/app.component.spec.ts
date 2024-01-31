3b39e1a8654dad26bef27513232587435588ce2d
anaceciliahope <gleidson.gmoura@gmail.com>
2024-01-31 17:38:15 -0300

    update home message

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, vox-choice');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Ana, estude java com dedicação');
   });
 });
