0d1f179a5eae4ef8c2d407d9d3d552c86a558e50
anaceciliahope <gleidson.gmoura@gmail.com>
2024-02-01 16:42:33 -0300

    update project reference

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'test-bootstrap' title`, () => {
+  it(`should have the 'vox-choice' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('test-bootstrap');
+    expect(app.title).toEqual('vox-choice');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, test-bootstrap');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, vox-choice');
   });
 });
